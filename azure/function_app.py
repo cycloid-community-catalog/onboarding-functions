2542114df7caaaffd13e11997ae5f4e9541709e4
olivier2t <80399237+olivier2t@users.noreply.github.com>
2024-12-14 11:44:37 +0100

    Update function_app.py

--- azure/function_app.py
+++ azure/function_app.py
@@ -10,42 +10,6 @@ import logging
 
 app = func.FunctionApp()
 
-@app.route(route="Locations", auth_level=func.AuthLevel.ANONYMOUS)
-def ResourceGroups(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Locations function triggered and processing a request.')
-
-    for location in locations:
-        print(location.name)
-    try:
-        # Acquire a credential object.
-        credential = DefaultAzureCredential()
-
-        # Retrieve subscription ID from environment variable.
-        subscription_id = os.environ.get("SUBSCRIPTION_ID", None)
-        
-        # Obtain the management object for resources.
-        subscription_client = SubscriptionClient(credential)
-
-        # Retrieve the list of locations
-        locations = subscription_client.subscriptions.list_locations(subscription_id)
-
-        response = []
-        for location in locations:
-            response.append(json.loads('{"label": "'+location.location+'","value": "'+location.name+'"}'))
-
-        return func.HttpResponse(
-            body=json.dumps(response),
-            status_code=200,
-            mimetype="application/json"
-        )
-
-    except Exception as e:
-        logging.error(f"Error occurred: {str(e)}")
-        return func.HttpResponse(
-            f"An error occurred: {str(e)}",
-            status_code=500
-        )
-
 @app.route(route="ResourceGroups", auth_level=func.AuthLevel.ANONYMOUS)
 def ResourceGroups(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('ResourceGroups function triggered and processing a request.')
